3dfb2536df04aca3c3490d34934e459404e9211d
Tyler Holdener <49045946+TyHold@users.noreply.github.com>
2019-05-05 11:38:16 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+      <form action="/" method='POST'>
+          <label>Rotate by:</label>
+          <input type="text" name='rot' label="Rotate by:" value='0'/>
+
+          <textarea rows="4" cols="50" name="text">{0}</textarea>
+          <input type="submit" value="Submit Query" />
+      
+      </form>
+    </body>
+</html>
+"""
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    text = request.form['text']
+    new_str = rotate_string(text, rot)
+    
+    return form.format(new_str)
+
+
+
+@app.route("/")
+def index():
+    return form.format('')
+
+app.run()
